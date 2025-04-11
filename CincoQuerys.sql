1780af2520ff2c7fc83511af4b2ab240ac53d57b
theozago <148823400+theozago@users.noreply.github.com>
2025-04-11 19:34:13 -0300

    Create CincoQuerys.sql

---
+++ CincoQuerys.sql
@@ -0,0 +1,33 @@
+SELECT 
+    a.nome AS aluno,
+    d.codigo AS cod_disciplina,
+    d.nome AS disciplina,
+    h.semestre,
+    h.nota,
+    h.aprovado
+FROM 
+    HistoricoEscolar h
+JOIN 
+    Aluno a ON a.id = h.aluno_id
+JOIN 
+    Disciplina d ON d.id = h.disciplina_id
+WHERE 
+    h.aluno_id IN (
+        SELECT 
+            h1.aluno_id
+        FROM 
+            HistoricoEscolar h1
+        WHERE 
+            h1.aprovado = FALSE
+        INTERSECT
+        SELECT 
+            h2.aluno_id
+        FROM 
+            HistoricoEscolar h2
+        WHERE 
+            h2.aprovado = TRUE
+    )
+ORDER BY 
+    h.aluno_id,
+    d.id,
+    h.semestre;
