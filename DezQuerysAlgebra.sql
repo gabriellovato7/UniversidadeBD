d6d49019f6fd9a24745f84ed4b9b2be95f6d3e81
theozago <148823400+theozago@users.noreply.github.com>
2025-04-14 18:31:46 -0300

    Update DezQuerysAlgebra.sql

--- DezQuerysAlgebra.sql
+++ DezQuerysAlgebra.sql
@@ -19,3 +19,16 @@ SELECT professor_id
 FROM DisciplinaLecionada
 GROUP BY professor_id
 HAVING COUNT(DISTINCT disciplina_id) > 1;
+
+--6.
+SELECT a.id, a.nome
+FROM Aluno a
+JOIN AlunoTCC atcc ON a.id = atcc.aluno_id
+JOIN TCC t ON atcc.tcc_id = t.id
+WHERE t.orientador_id = 1;
+
+--7.
+SELECT a.nome
+FROM Aluno a
+JOIN Curso c ON a.curso_id = c.id
+WHERE c.nome IN ('Ciência da Computação', 'Engenharia Elétrica');
