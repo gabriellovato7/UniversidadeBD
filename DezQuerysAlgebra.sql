4f3c9e6c14e6ad2d5c829560dd5187cd2e9601be
theozago <148823400+theozago@users.noreply.github.com>
2025-04-14 18:31:04 -0300

    Update DezQuerysAlgebra.sql

--- DezQuerysAlgebra.sql
+++ DezQuerysAlgebra.sql
@@ -6,3 +6,16 @@ SELECT id, nome FROM Professor;
 
 --3.
 SELECT nome, orcamento FROM Departamento WHERE orcamento > 50000;
+
+--4.
+SELECT DISTINCT a.nome
+FROM Aluno a
+JOIN HistoricoEscolar h ON a.id = h.aluno_id
+JOIN Disciplina d ON h.disciplina_id = d.id
+WHERE d.nome = 'Banco de Dados';
+
+--5.
+SELECT professor_id
+FROM DisciplinaLecionada
+GROUP BY professor_id
+HAVING COUNT(DISTINCT disciplina_id) > 1;
